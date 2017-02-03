b00eb38310b9366c53d2ea0b8e79a46f7a1d5530
Fujie Chen <imaple7@gmail.com>
2017-02-03 00:03:26 -0600

    Create MineSweeper Game

---
+++ CSharpTest/MineSweeper/Form_Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    public partial class Form_Main : Form
+    {
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
